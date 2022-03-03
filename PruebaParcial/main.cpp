d079e33808f0c754321481156c6129b720b47d50
MeyglinRosales <mrosaleso3@miumg.edu.gt>
2022-03-03 16:05:23 -0600

    Se ha agregado nuevo cambio

--- PruebaParcial/main.cpp
+++ PruebaParcial/main.cpp
@@ -1,15 +1,20 @@
 # incluir  <iostream>
 
-principal ()
+main()
 {
 int m1[3][3] = {{ 1 , 1 , 1 }, { 2 , 2 , 2 }, { 3 , 3 , 3 }};
-intm2 [3][3];
-entero x, y;
-para (x= 0 ; x < 3; x++)
-para (y= 0 ; y < 3; y++)
-si (m1[x][y]% 2 == 0 )
-m2[x][y] = 0 ;
-demás
-m2[x][y] = 1 ;
-printf ( " %d , %d , %d " , m2[ 0 ][ 0 ], m2[ 1 ][ 1 ], m2[ 2 ][ 2 ]);
+int m2 [3][3];
+int x, y;
+for(x= 0 ; x < 3; x++)
+for(y= 0 ; y < 3; y++)
+if (m1[x][y]% 2 == 0 )
+    {
+        m2[x][y] = 0 ;
+    }
+    else
+    if(m2[x][y] = 1)
+        {
+            cout << ( " %d , %d , %d " , m2[ 0 ][ 0 ], m2[ 1 ][ 1 ], m2[ 2 ][ 2 ]) << endl;
+        }
+    return 0;
 }
