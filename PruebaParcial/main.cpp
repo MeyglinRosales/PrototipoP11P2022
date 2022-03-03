ff053fc263bc57105524405be33e3f851b40e99f
MeyglinRosales <mrosaleso3@miumg.edu.gt>
2022-03-03 15:59:25 -0600

    Se ha agregado cambio

--- PruebaParcial/main.cpp
+++ PruebaParcial/main.cpp
@@ -1,15 +1,19 @@
-# incluir  < stdio.h >
-# definir  N  3
+# incluir  <iostream>
+
 principal ()
 {
-int m1[N][N] = {{ 1 , 1 , 1 }, { 2 , 2 , 2 }, { 3 , 3 , 3 }};
-intm2 [N][N];
-entero x, y;
-para (x= 0 ; x < N; x++)
-para (y= 0 ; y < N; y++)
-si (m1[x][y]% 2 == 0 )
-m2[x][y] = 0 ;
-demás
-m2[x][y] = 1 ;
-printf ( " %d , %d , %d " , m2[ 0 ][ 0 ], m2[ 1 ][ 1 ], m2[ 2 ][ 2 ]);
+int m1[3][3] = {{ 1 , 1 , 1 }, { 2 , 2 , 2 }, { 3 , 3 , 3 }};
+int m2 [3][3];
+int x, y;
+for(x= 0 ; x < 3; x++)
+for(y= 0 ; y < 3; y++)
+if (m1[x][y]% 2 == 0 )
+    {
+        m2[x][y] = 0 ;
+    }
+    else
+    if(m2[x][y] = 1)
+        {
+            printl ( " %d , %d , %d " , m2[ 0 ][ 0 ], m2[ 1 ][ 1 ], m2[ 2 ][ 2 ]);
+        }
 }
